85d7a7f689d0d635379734fc1fd1cdfdc20e7942
Sandreno23 <sandrenopatria@gmail.com>
2026-03-31 20:59:25 +0700

    first

--- src/components/SmallLetter.jsx
+++ src/components/SmallLetter.jsx
@@ -78,7 +78,7 @@ const SmallLetter = () => {
         >
             x
         </div>
-        <p>Yo brocaccho,<br /><br />Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud <br /><br />Rajib</p>
+        <p>Yo Dude,<br /><br />Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud <br /><br />Rajib</p>
     </div>
 
     {/* --- NEW SVG DIARY PAGE DECORATION --- */}
